eb18d9f303697019fbb7701e5b0d40084936a5a0
Zahid Mzhmm <zahidmzhmm@gmail.com>
2022-05-08 23:18:27 +0600

    Header Footer Pages Created

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from "../layouts/Header";
+import Footer from "../layouts/Footer";
+
+const Home = () => {
+    return (
+        <>
+            <Header/>
+            <Footer/>
+        </>
+    );
+};
+
+export default Home;
