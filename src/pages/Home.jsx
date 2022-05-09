d8e14fbe69b5b23270e7eb125d22837207303cf2
Zahid Mzhmm <zahidmzhmm@gmail.com>
2022-05-09 22:16:52 +0600

    Home Finished

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,6 +13,16 @@ const Home = () => {
             <RecentNotices items={items} type={1}/>
             <div className="mt-main"/>
             <RecentJobs items={items} type={2}/>
+            <div className="mt-main"/>
+            <div className="postjob-alert p-4 rounded-main">
+                <h4 className="text-center heading mb-0 mt-1">Want to post a Job listing?</h4>
+                <p className="text-center text mt-3">
+                    Get registered on <b><a href="https://jobsicle.mv" className="link font-color-white">www.jobsicle.mv</a></b> and
+                    post your jobs there.
+                    It will instantly get published in our
+                    job section
+                </p>
+            </div>
             <div className="pt-main"/>
         </>
     );
