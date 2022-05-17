9701e27fd9ad7e36c3dd4c11fe32024510dab968
Zahid Mzhmm <zahidmzhmm@gmail.com>
2022-05-17 15:48:40 +0600

    Expired Option Added

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -4,8 +4,9 @@ import {Link} from "react-router-dom";
 import {ReqCRUD} from "../request";
 import {toast} from "react-toastify";
 import {alertOptions} from "../config";
+import moment from 'moment';
 
-const ListingItem = ({data,setUpdate}) => {
+const ListingItem = ({data, setUpdate}) => {
     const forceClose = (id) => {
         ReqCRUD('user/force-close/' + id, 'get', localStorage.getItem('token')).then((data) => {
             if (parseInt(data.status) === 200) {
@@ -22,6 +23,15 @@ const ListingItem = ({data,setUpdate}) => {
             }
         })
     }
+    const getDaysBetweenDates = function (startDate) {
+        let now = startDate.clone(), dates = [];
+
+        while (now.isSameOrBefore(moment(new Date()))) {
+            dates.push(now.format('DD'));
+            now.add(1, 'days');
+        }
+        return dates.length;
+    };
     return (
         <>
             <div
@@ -45,22 +55,25 @@ const ListingItem = ({data,setUpdate}) => {
                     <button onClick={(e) => deleteList(data.id)}
                             className="btn btn-outline-main outline-text py-1 px-2 btn-sm ms-2">delete
                     </button>
-                    {data.expired_at === null
-                        ?
-                        <button onClick={(e) => forceClose(data.id)}
-                                className="btn btn-outline-main outline-text py-1 px-2 btn-sm ms-2 btn-werf">force
-                            close
-                        </button> : <Link to={`/report-listings/${data.id}`}
-                                          className="btn btn-outline-main outline-text py-1 px-2 btn-werf btn-sm ms-2">repost
-                        </Link>}
-                    {data.expired_at === null
-                        ?
-                        <span
-                            className={`${parseInt(data.status) === 1 && "alert-color-3"} ${parseInt(data.status) === 2 && "alert-color-1"} ms-2 text-xs text-bold wnitem8`}>
-                                                    {parseInt(data.status) === 1 && "pending approval"}{parseInt(data.status) === 2 && "published"}
-                                                </span>
-                        : <span
-                            className={`alert-color-2 ms-2 text-xs text-bold wnitem8`}> expired </span>}
+                    {getDaysBetweenDates(moment(data.expired_at)) > 0 ?
+                        <>
+                            <Link to={`/report-listings/${data.id}`}
+                                  className="btn btn-outline-main outline-text py-1 px-2 btn-werf btn-sm ms-2">repost
+                            </Link>
+                            <span className={`alert-color-2 ms-2 text-xs text-bold wnitem8`}> expired </span>
+                        </>
+                        :
+                        <>
+                            <button onClick={(e) => forceClose(data.id)}
+                                    className="btn btn-outline-main outline-text py-1 px-2 btn-sm ms-2 btn-werf">force
+                                close
+                            </button>
+                            <span
+                                className={`${parseInt(data.status) === 1 && "alert-color-3"} ${parseInt(data.status) === 2 && "alert-color-1"} ms-2 text-xs text-bold wnitem8`}>
+                                    {parseInt(data.status) === 1 && "pending approval"}{parseInt(data.status) === 2 && "published"}
+                                </span>
+                        </>
+                    }
                 </div>
             </div>
         </>
